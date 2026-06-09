7c8e99a4583348652222dde7351d0a7651277294
Benedict Sotalbo <Benedict>
2026-06-09 21:54:24 +0800

    feat(frontend): send selected academic year in API request headers

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,11 +4,21 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
 
-instance.interceptors.request.use(config => {
+instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
+  const academicYearId =
+    localStorage.getItem("viewedAcademicYearId");
+
+  if (academicYearId) {
+    config.headers["x-academic-year-id"] =
+      academicYearId;
+  }
+
   return config;
 });
 
