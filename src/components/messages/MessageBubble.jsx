5d0fe43c1c4f1db56b6806a6e9e4dc12200329be
Benedict Sotalbo <Benedict>
2026-07-21 12:07:46 +0800

    fix(chat): correct ReactionIcon import path

--- src/components/messages/MessageBubble.jsx
+++ src/components/messages/MessageBubble.jsx
@@ -1,6 +1,6 @@
 import { useCallback, memo } from "react";
 import { Check, CheckCheck, Clock, AlertCircle, SmilePlus } from "lucide-react";
-import Avatar from "../../ui/Avatar";
+import Avatar from "../ui/Avatar";
 import ReactionIcon from "../ui/ReactionIcon";
 import AttachmentBlock from "./AttachmentBlock";
 import { getReactionMeta } from "../../constants/reactions";
